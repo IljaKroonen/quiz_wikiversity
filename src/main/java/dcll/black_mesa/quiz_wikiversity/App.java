6f59a7bf02d3bdc718d5b89c1889ad7009e01b61
IljaKroonen <ilja.kroonen@gmail.com>
2014-03-17 11:23:28 +0100

    Added checkstyle profile to the project. Files from tsaap are ignored

--- src/main/java/dcll/black_mesa/quiz_wikiversity/App.java
+++ src/main/java/dcll/black_mesa/quiz_wikiversity/App.java
@@ -2,12 +2,10 @@ package dcll.black_mesa.quiz_wikiversity;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
---
+++ src/main/java/dcll/black_mesa/quiz_wikiversity/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the parser for the Wikiversity format.
+ */
+package dcll.black_mesa.quiz_wikiversity;
