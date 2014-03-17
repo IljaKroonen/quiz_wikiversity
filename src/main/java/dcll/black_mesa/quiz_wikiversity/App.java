a47032fb0b342748b0d1351fbed7ab78b290538e
IljaKroonen <ilja.kroonen@gmail.com>
2014-03-17 11:08:45 +0100

    Created project

---
+++ src/main/java/dcll/black_mesa/quiz_wikiversity/App.java
@@ -0,0 +1,13 @@
+package dcll.black_mesa.quiz_wikiversity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
