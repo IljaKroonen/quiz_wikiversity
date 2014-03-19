b44000ec8750dce9b6d52e321ed3e152dc6c9158
llDavidll <d.lericolais@gmail.com>
2014-03-19 09:43:27 +0100

    Fixes #1 Add the cobertura plugin and the first test for DefaultAnswer

---
+++ src/test/java/org/tssap/questions/impl/tests/DefaultAnswerTest.java
@@ -0,0 +1,69 @@
+package org.tssap.questions.impl.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.tsaap.questions.impl.DefaultAnswer;
+
+public class DefaultAnswerTest {
+
+	private DefaultAnswer mDefaultAnswer;
+
+	@Before
+	public void setUp() {
+
+		mDefaultAnswer = new DefaultAnswer();
+	}
+
+	@Test
+	public void attributesTest() {
+
+		mDefaultAnswer.setTextValue("TextValue");
+		assertEquals("TextValue", mDefaultAnswer.getTextValue());
+		
+		mDefaultAnswer.setPercentCredit(0.5f);
+		assertEquals(0.5f, mDefaultAnswer.getPercentCredit(), 0f);
+		
+		mDefaultAnswer.setIdentifier("Identifier");
+		assertEquals("Identifier", mDefaultAnswer.getIdentifier());
+		
+		mDefaultAnswer.setFeedback("Feedback");
+		assertEquals("Feedback", mDefaultAnswer.getFeedBack());
+		
+	}
+	
+	@Test
+	public void hashCodeTest(){
+		
+		mDefaultAnswer.setIdentifier("Identifier");
+		int hashCode = mDefaultAnswer.hashCode();
+		assertEquals(hashCode, mDefaultAnswer.hashCode());
+	}
+	
+	@Test
+	public void equalsTest(){
+		
+		mDefaultAnswer.setIdentifier("Identifier1");
+		
+		DefaultAnswer defaultAnswer2 = new DefaultAnswer();
+		defaultAnswer2.setIdentifier("Identifier2");
+		
+		// Same object
+		assertTrue(mDefaultAnswer.equals(mDefaultAnswer));
+		
+		// Null object
+		assertFalse(mDefaultAnswer.equals(null));
+		
+		// Different object
+		assertFalse(mDefaultAnswer.equals(new Integer(1)));
+		
+		// Other question with a different identifier
+		assertFalse(mDefaultAnswer.equals(defaultAnswer2));
+		
+		defaultAnswer2.setIdentifier("Identifier1");
+		// Other question with the same identifier
+		assertTrue(mDefaultAnswer.equals(defaultAnswer2));
+	}
+	
+}
